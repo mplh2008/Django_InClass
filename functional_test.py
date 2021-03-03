d5819df231439cbe128606528092450c51fa6ef2
Sohail Ahmed <sneakersaool@gmail.com>
2021-03-02 16:28:48 -0800

    Part 1 Chapter 2 Completion

--- functional_test.py
+++ functional_test.py
@@ -1,26 +1,39 @@
 from selenium import webdriver
+import unittest
 
-#Sohail heard about a cool online app.
-#He decided to check its homepage...
-browser = webdriver.Firefox()
 
-#He noticed a page title and header contained a to-do list
-assert 'To-Do' in browser.title
+class newVisitorTest(unittest.TestCase):
 
-#He is prompted for a to-do list
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-#He types "Buy more Jordans" into a test box
+	def tearDown(self):
+		self.browser.quit()
 
-#He hits enter and the page updates
-#"1: Buy more Jordans"
+	def test_startList_retrieveLater(self):
+		#Sohail heard about a cool online app.
+		#He decided to check its homepage...
+		self.browser.get('http://localhost:8000')
 
-#There is an additional test box reqesting another item to add to the list
+		#He noticed a page title and header contained a to-do list
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
 
-#He enters "Ask Danielle how the activity is going"(Sohail is very helpful XD)
+		#He is prompted for a to-do list
 
-#The page updates and shows bot items on his list
+		#He types "Buy more Jordans" into a test box
 
-#He wonders if the list is saved, luckily he sees a text box that has a link to his generated list
-#He happily saves the list and turns on the TV
+		#He hits enter and the page updates
+		#"1: Buy more Jordans"
 
-browser.quit()
+		#There is an additional test box reqesting another item to add to the list
+
+		#He enters "Ask Danielle how the activity is going"(Sohail is very helpful XD)
+
+		#The page updates and shows bot items on his list
+
+		#He wonders if the list is saved, luckily he sees a text box that has a link to his generated list
+		#He happily saves the list and turns on the TV
+
+if __name__ == '__main__':
+	unittest.main()
